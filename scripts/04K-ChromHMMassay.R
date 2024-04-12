1899d69e4d4f8d9512e2bf19f41b3a4a5eff05b0
AlexandrePelletier <admpelletier@gmail.com>
2024-04-12 18:02:05 -0400

    notebook for fgsea analysis

--- scripts/04K-ChromHMMassay.R
+++ scripts/04K-ChromHMMassay.R
@@ -11,54 +11,32 @@ source('../../utils/r_utils.R')
 #in https://egg2.wustl.edu/roadmap/web_portal/chr_state_learning.html#exp_18state
 #for 1chr
 system('wget -O ref-data/ChromHMM_brain_dlpfc/E073_18_core_K27ac_chr1_statebyline.txt.gz https://egg2.wustl.edu/roadmap/data/byFileType/chromhmmSegmentations/ChmmModels/core_K27ac/jointModel/final/STATEBYLINE/E073_18_core_K27ac_chr1_statebyline.txt.gz')
+system('wget -O ref-data/ChromHMM_brain_dlpfc/E073_18_core_K27ac_hg38lift_mnemonics.bed.gz https://egg2.wustl.edu/roadmap/data/byFileType/chromhmmSegmentations/ChmmModels/core_K27ac/jointModel/final/E073_18_core_K27ac_hg38lift_mnemonics.bed.gz')
 
-'https://egg2.wustl.edu/roadmap/data/byFileType/chromhmmSegmentations/ChmmModels/core_K27ac/jointModel/final/STATEBYLINE/E073_18_core_K27ac_chr1_statebyline.txt.gz'
 
-chr1<-fread('E073_18_core_K27ac_chr1_statebyline.txt.gz',skip = 1,col.names = 'state')
-unique(chr1$state)
-#add pos
-chr1[,chr:='chr1'][,start:=(0:(.N-1))*200][,end:=(1:.N)*200-1]
-#add states anno
-states<-fread('ref-data/chromHMM18states.csv')
-chr1<-merge(chr1,states)
 
-#for all 
-#download them
-cmds=sapply(c(1:22,'X','Y'),function(c)paste('wget -O',ps('ref-data/ChromHMM_brain_dlpfc/E073_18_core_K27ac_chr',c,'_statebyline.txt.gz'),
-                                             ps('https://egg2.wustl.edu/roadmap/data/byFileType/chromhmmSegmentations/ChmmModels/core_K27ac/jointModel/final/STATEBYLINE/E073_18_core_K27ac_chr',c,'_statebyline.txt.gz')))
-sapply(cmds, function(cmd)system(cmd))
+chromst<-fread('ref-data/ChromHMM_brain_dlpfc/E073_18_core_K27ac_hg38lift_mnemonics.bed.gz',col.names = c('chr','start','end','state_name'))
 
-#open add pos, anno and bind them
-states<-fread('ref-data/chromHMM18states.csv')
-
-chromst<-rbindlist(lapply(paste0('chr',c(1:22,'X','Y')), function(chr){
-  chrx<-fread(fp('ref-data/ChromHMM_brain_dlpfc/',ps('E073_18_core_K27ac_',chr,'_statebyline.txt.gz')),skip = 1,col.names = 'state') 
-  #add pos
-  chrx[,chr:=chr][,start:=(0:(.N-1))*200][,end:=(1:.N)*200-1]
-  #add states anno
-  chrx<-merge(chrx,states)
-  return(chrx)
-}))
-
-class(chromst$start)
-chromst[chr=='chr1']
-chromst[,start:=as.numeric(start)]
-fwrite(chromst[order(chr,start)],'ref-data/ChromHMM_brain_dlpfc/chromHMM_18states_200pb_bin_all_chr.csv.gz')
-
-fwrite(chromst[order(chr,start)][,.(chr,start,end,state)],
-       'ref-data/ChromHMM_brain_dlpfc/chromHMM_18states_200pb_bin_all_chr.bed.gz',
-       sep = '\t',col.names = FALSE,scipen = 999)
+chromst[,state:=str_extract(state_name,'^[0-9]+')]
+chromst[,name:=str_remove(state_name,'^[0-9]+_')]
+fwrite(chromst,'ref-data/ChromHMM_brain_dlpfc/chromHMM_18states_all_chr.csv.gz')
+fwrite(chromst,'ref-data/ChromHMM_brain_dlpfc/chromHMM_18states_all_chr.bed.gz',
+       col.names = F,scipen = 999,sep='\t')
+unique(chromst$chr)
+fwrite(chromst[chr!='ChrM'],'ref-data/ChromHMM_brain_dlpfc/chromHMM_18states_noChrM.bed.gz',
+       col.names = F,scipen = 999,sep='\t')
 
 #create bed file for each state
 out_chromref<-'ref-data/ChromHMM_brain_dlpfc/bed_file_per_state'
 dir.create(out_chromref)
 
 for(s in unique(chromst$state)){
-  fwrite(chromst[state==s][order(chr,start)][,.(chr,start,end,state)],
-         fp(out_chromref,paste0('state',s,'_200pb_bin.bed.gz')),
+  fwrite(chromst[state==s][order(chr,start)][,.(chr,start,end,state_name)],
+         fp(out_chromref,paste0('state',s,'.bed.gz')),
          sep = '\t',col.names = FALSE,scipen = 999)
   
 }
+
 #count by cells for each cell type
 chromst<-fread('../APOE4_aging/ref-data/ChromHMM_brain_dlpfc/chromHMM_18states_200pb_bin_all_chr.csv.gz')
 
@@ -102,8 +80,8 @@ saveRDS(astro,'../singlecell-brain/outputs/04-ROSMAP_MIT_ATAC/Astro.rds')
 
 #For all cell type####
 #run 04Ki
-CreateJobForRfile('scripts/04Ki-CountChromHMM.R',nThreads = 16)
-RunQsub('scripts/04Ki-CountChromHMM.R',job_name = 'countCHStates')
+CreateJobForRfile('../singlecell-brain/scripts/04Ki-CountChromHMM.R',nThreads = 16)
+RunQsub('../singlecell-brain/scripts/04Ki-CountChromHMM.R',job_name = 'countCHStates')
 
 
 
--- scripts/04Ki-CountChromHMM.R
+++ scripts/04Ki-CountChromHMM.R
@@ -10,14 +10,13 @@ source('../../utils/r_utils.R')
 states<-fread('ref-data/chromHMM18states.csv')
 
 
-
 celltype_files<-list.files(out,'Ast|Exc|Inh|Mic|Oligo|OPC|VLMC',full.names = T)
 for(file in celltype_files){
   message(file)
   celltype<-readRDS(file)
   for(s in states$state){
     message('state ',s)
-    state<-fread(ps('ref-data/ChromHMM_brain_dlpfc/bed_file_per_state/state',s,'_200pb_bin.bed.gz'),col.names = c('chr','start','end','state'))
+    state<-fread(ps('ref-data/ChromHMM_brain_dlpfc/bed_file_per_state/state',s,'.bed.gz'),col.names = c('chr','start','end','state_name'))
     state_gr<-makeGRangesFromDataFrame(state)
     count<-CountsInRegion(celltype,assay = 'peaksCT',regions = state_gr)
     celltype[[paste0('n',states[state==s]$name,'_ChromHMM')]]<-count
